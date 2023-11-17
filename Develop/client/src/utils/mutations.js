dd2fadfa482e9f196712e4f5cfa391ba8e483926
Jenae Luthi <jenaeluthi@gmail.com>
2023-11-17 16:00:21 -0700

    had a problem with my git hub repo and had to save work in a different file and then push it

---
+++ Develop/client/src/utils/mutations.js
@@ -0,0 +1,63 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVEBOOKS = gql`
+  mutation saveBooks ($bookData: BookInput!) {
+    saveBooks(bookData: $bookData) {
+      _id
+      username
+      email
+       savedBooks {
+        authors
+        description
+        bookId
+        image
+        link
+        title
+        
+      }
+    }
+  }
+`;
+
+export const REMOVEBOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+        _id
+      username
+      email
+      savedBooks {
+        authors
+        description
+        bookId
+        image
+        link
+        title
+        
+      }
+    }
+  }
+`;
